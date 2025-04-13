3b6ebe7136900607762d8d8d165202536d756cc3
JulianPollos <kreikejulian@gmail.com>
2025-04-13 14:53:07 +0200

    af

---
+++ main.js
@@ -0,0 +1,49 @@
+let currentPost = 0;       
+const maxPosts = 10;       
+let allPosts = [];         
+
+function readJson() {
+    fetch("posts.json")
+        .then(response => response.json())
+        .then(data => {
+            allPosts = data;   
+            addJson();         
+        });
+}
+
+function addJson() {
+    const feed = document.getElementById("posts");
+
+    for (let i = currentPost; i < currentPost + maxPosts && i < allPosts.length; i++) {
+        const post = document.createElement("div");
+        post.className = "post";
+
+        const username = document.createElement("p");
+        username.className = "username";
+        username.textContent = allPosts[i].username;  
+
+        const content = document.createElement("p");
+        content.className = "post_content";
+        content.textContent = allPosts[i].post_content;
+
+        post.appendChild(username);
+        post.appendChild(content);
+        feed.appendChild(post);
+    }
+
+    currentPost += maxPosts;
+
+    const end = document.getElementById("end");
+
+    const endObserver = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+            readJson();
+            
+        }
+    });
+
+    endObserver.observe(end);
+
+}
+
+readJson();
